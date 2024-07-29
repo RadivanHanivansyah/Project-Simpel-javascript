a77181bce12b2847cba2c3d694556d35c6bd2afd
RadivanHanivansyah <radivan100@gmail.com>
2024-07-29 12:18:23 +0700

    Menambahkan script js (hex) pada color flipper

--- Color flipper/script.js
+++ Color flipper/script.js
@@ -1,13 +1,29 @@
 //script rgb
+// const container = document.querySelector('.hex');
+// const tombol = document.querySelector('button');
+// tombol.addEventListener('click', function () {
+//     const max = Math.max(255);
+//     const min = Math.min(0);
+//     const red = Math.floor((Math.random() * max) + min);
+//     const green = Math.floor((Math.random() * max) + min);
+//     const blue = Math.floor((Math.random() * max) + min);
+//     document.body.style.backgroundColor = "rgb(" + red + "," + green + ", " + blue + ")";
+//     container.innerHTML = "rgb(" + red + ", " + green + ", " + blue + ");"
+//     tombol.style.fontWeight = "900";
+// })
+//script hex
 const container = document.querySelector('.hex');
 const tombol = document.querySelector('button');
-tombol.addEventListener('click',function(){
-const max = Math.max(255);
-const min = Math.min(0);
-const red = Math.floor((Math.random() * max) + min);
-const green = Math.floor((Math.random() * max) + min);
-const blue = Math.floor((Math.random() * max) + min);
-document.body.style.backgroundColor = "rgb("+red+","+green+", "+blue+")";
-container.innerHTML = "rgb("+red+", "+green+", "+blue+");"
-tombol.style.fontWeight = "900";
-})
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+tombol.addEventListener("click", function () {
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()];
+    }
+    container.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
