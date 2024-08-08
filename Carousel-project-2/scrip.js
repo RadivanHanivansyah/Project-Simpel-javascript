348e9dc4f2c9e4461f56e652c1977e4817fa4563
RadivanHanivansyah <radivan100@gmail.com>
2024-08-08 21:30:12 +0700

    membuat data string menjadi int pada proyek carousel ke-2

--- Carousel-project-2/scrip.js
+++ Carousel-project-2/scrip.js
@@ -2,31 +2,31 @@ const data = [
     {
         img: './img/img-1.png',
         name: 'Slick Sneakers',
-        price: '$79.9',
+        price: 79.9,
         imgStar: './img/star-1.png',
-        numberReview: '31 Review(s)'
+        numberReview: 31
 
     },
     {
         img: './img/img-2.png',
         name: 'Stride Flex',
-        price: '$89.99',
+        price: 89.99,
         imgStar: './img/star-2.png',
-        numberReview: '40 Review(s)'
+        numberReview: 40
     },
     {
         img: './img/img-3.png',
         name: 'Luxe Step',
-        price: '$129.99',
+        price: 129.99,
         imgStar: './img/star-3.png',
-        numberReview: '50 Review(s)'
+        numberReview: 50
     },
     {
         img: './img/img-4.png',
         name: 'AeroGrip Pro',
-        price: '$99.99',
+        price: 99.99,
         imgStar: './img/star-4.png',
-        numberReview: '70 Review(s)'
+        numberReview: 70
     }
 ];
 
@@ -43,9 +43,9 @@ function showData() {
     let arrData = data[orderArray];
     img.src = arrData.img;
     namaSepatu.textContent = arrData.name;
-    price.textContent = arrData.price;
+    price.textContent = '$' + arrData.price;
     reviewStar.src = arrData.imgStar;
-    reviewtotal.textContent = arrData.numberReview;
+    reviewtotal.textContent = arrData.numberReview + ' ' + 'Reviews(s)';
 }
 left.addEventListener('click', function () {
     --orderArray;
